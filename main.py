a3b52e759503cad27dd1ab2f9677287a283e8d68
nfongHang <fngai@kellettschool.com>
2024-10-04 11:40:19 +0800

    enable gitdoc

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ class Construct:
         self.rect=pygame.Rect(self.x,self.y,self.size[0],self.size[1])
     
 #test
+#test 2.1
 class Entity(Construct):
     def __init__(self, size, x, y):
         super().__init__(size,x,y)
